afaed70581ec2012a1c5985991c4e33e4c1f5544
Andi1218 <Andi-lenz@web.de>
2017-01-18 14:25:13 +0100

    min max linien eingefügt

--- Website/js/charts.js
+++ Website/js/charts.js
@@ -1,24 +1,27 @@
 /* Chart current Voltage */
 
-
+  /*
      $(document).ready(function () {
-      var data = []; 
+      var data = ["Datum, Funktion, Linie/n"]; 
       var dataLength = 15;
       var t = new Date();
       for (var i = 10; i >= 0; i--) {
         var x = new Date(t.getTime() - i * 1000);
-        data.push([x, Math.random()]);
+        var z = 1.1;
+        data.push(x, Math.random(),z"/n");
+       
       }
 
       var g = new Dygraph("div_g", data,
                           {
+                            
                             drawPoints: true,
-                            showRoller: true,
+                            showRoller: false,
                             valueRange: [0.0, 1.2],
                             labels: ['Time', 'Random']
                           });
       // It sucks that these things aren't objects, and we need to store state in window.
-      window.intervalId = setInterval(function() {
+     window.intervalId = setInterval(function() {
         var x = new Date();  // current time
         var y = Math.random();
         data.push([x, y]);
@@ -26,6 +29,67 @@
          if (data.length > dataLength) {
         data.shift();
         };
-      }, 1000);
+      }, 1000);      
+    }            
+);       */
+
+
+ $(document).ready(function () {
+         var dataLenght = 15;
+         var r = "date, values,line max,line min\n";
+         var t = new Date();   
+         
+          for (var i=1; i<=10; i++) {
+                r += new Date(t.getTime() - i * 1000);
+                r += "," + Math.random();
+                r += "," + 1.1;
+                r += "," + 0.9;
+                r += "\n";
+                }    
+      
+      
+      var g = new Dygraph(
+              "div_g", r,
+              {
+                
+                series: {
+                  'values': {
+                    strokeWidth: 0.0,
+                    drawPoints: true,
+                    pointSize: 4,
+                    highlightCircleSize: 6
+                  },
+                  'line max': {
+                    strokeWidth: 1.0,
+                    drawPoints: true,
+                    pointSize: 1.5
+                  },
+                  'line min': {
+                    strokeWidth: 3,
+                    highlightCircleSize: 10
+                  }
+                }
+              }
+          );  
+          
+          window.intervalId = setInterval(function() {
+       
+         
+          r += new Date();
+          r += "," + Math.random();
+          r += "," + 1.1;
+          r += "," + 0.9;
+          r += "\n";
+          
+          
+      
+          g.updateOptions( { 'file': r } );  
+          
+          
+              if (r.length > dataLength) {
+              r.shift();
+              };        
+              
+         }, 1000);
     }
-);
+); 
