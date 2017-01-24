170de636c6061496e7d4391346c53cd58520727c
paulm91 <paul.martin1991@googlemail.com>
2017-01-24 10:31:00 +0100

    Analyse historischer Daten

--- analyse.py
+++ analyse.py
@@ -5,19 +5,24 @@ Created on Wed Jan 11 16:08:28 2017
 @author: Paul-G
 """
 
-def analyse(dict_list):
-    dict = dict_list[0]
+def analyse(pq_data):
+    '''Analyses live Data: frequency, voltage, THD, harmonics'''
+    data_dict = pq_data[0]
                
     '''Analyses frequency and checks if measured data is confom with EN 50160'''
-#            frequency_average_10s = [10]
+#            frequency_average_10s = []
 #            while True:
 #                for i in frequency_average:
-#                frequency = dict.get("port_19050")  
+#                frequency = data_dict.get("port_19050")
+                
 #                frequency_average.append(frequency)
+#                frequency_average.delete[10]
 
-    freqeuency_average_10s = dict.get("port_800")
+
+
+    freqeuency_average_10s = data_dict.get("port_800")
                     
-    if freqeuency_average_10s < 49.999 or freqeuency_average_10s > 50.001:
+    if freqeuency_average_10s < 47.5 or freqeuency_average_10s > 52:
         frequency = "bad"
     elif freqeuency_average_10s < 49.5 or freqeuency_average_10s > 50.5:
         frequency = "critical"
@@ -25,9 +30,9 @@ def analyse(dict_list):
         frequency = "okay"
             
     '''Analyses voltage and checks if measured data is confom with EN 50160'''
-    voltage_L1_average =  dict.get("port_1728")
-    voltage_L2_average =  dict.get("port_1730")
-    voltage_L3_average =  dict.get("port_1732")
+    voltage_L1_average =  data_dict.get("port_1728")
+    voltage_L2_average =  data_dict.get("port_1730")
+    voltage_L3_average =  data_dict.get("port_1732")
             
     if voltage_L1_average < 208 or voltage_L1_average > 254:
         voltage_L1 = "critical"
@@ -45,9 +50,9 @@ def analyse(dict_list):
         voltage_L3 = "okay"
                                 
     '''Analyses Total Harmonics Distortion (THD) and checks if measured data is confom with EN 50160'''
-    THD_L1 = dict.get("port_836")
-    THD_L2 = dict.get("port_868")
-    THD_L3 = dict.get("port_840")
+    THD_L1 = data_dict.get("port_836")
+    THD_L2 = data_dict.get("port_868")
+    THD_L3 = data_dict.get("port_840")
             
     if THD_L1  >= 8:
         THD_L1 = "bad"
@@ -65,45 +70,49 @@ def analyse(dict_list):
         THD_L3 = "okay"
             
     '''Analyses most important harmonics (5th & 7th) and checks if data is conform with EN 50160'''
-    dict = dict_list[1]
+#    data_dict = pq_data[1]
 
     '''Harmonics U L1,L2,L3: Maximum of mean value'''
-    harmonic_5th_U1 = dict.get("port_2606")
-    harmonic_7th_U1 = dict.get("port_2610")
+    harmonic_5th_U1 = data_dict.get("port_1008")
+    harmonic_7th_U1 = data_dict.get("port_1012")
+    voltage_U1 = data_dict.get("port_808") 
             
-    harmonic_5th_U2 = dict.get("port_2686")
-    harmonic_7th_U2 = dict.get("port_2690")
+    harmonic_5th_U2 = data_dict.get("port_1088")
+    harmonic_7th_U2 = data_dict.get("port_1092")
+    voltage_U2 = data_dict.get("port_810") 
     
-    harmonic_5th_U3 = dict.get("port_2766")
-    harmonic_7th_U3 = dict.get("port_2770")
+    '''inacceptable values from janitza for L3'''
+    harmonic_5th_U3 = data_dict.get("port_1168")
+    harmonic_7th_U3 = data_dict.get("port_1172")
+    voltage_U3 = data_dict.get("port_812") 
     
     
-    if harmonic_5th_U1/231 > 0.06:
+    if harmonic_5th_U1/voltage_U1 > 0.06:
         harmonic_5th_U1 = "bad"
     else:
         harmonic_5th_U1 = "okay"
     
-    if harmonic_7th_U1/231 > 0.05:
+    if harmonic_7th_U1/voltage_U1 > 0.05:
         harmonic_7th_U1 = "bad"
     else:
         harmonic_7th_U1 = "okay"
         
-    if harmonic_5th_U2/231 > 0.06:
+    if harmonic_5th_U2/voltage_U2 > 0.06:
         harmonic_5th_U2 = "bad"
     else:
         harmonic_5th_U2 = "okay"
    
-    if harmonic_7th_U2/231 > 0.05:
+    if harmonic_7th_U2/voltage_U2 > 0.05:
         harmonic_7th_U2 = "bad"
     else:
         harmonic_7th_U2 = "okay"
         
-    if harmonic_5th_U3/231 > 0.06:
+    if harmonic_5th_U3/voltage_U3 > 0.06:
         harmonic_5th_U3 = "bad"
     else:
         harmonic_5th_U3 = "okay"
     
-    if harmonic_7th_U3/231 > 0.05:
+    if harmonic_7th_U3/voltage_U3 > 0.05:
         harmonic_7th_U3 = "bad"
     else:
         harmonic_7th_U3 = "okay"
