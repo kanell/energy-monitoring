06d016d5b81f519271161763a3cebfbe5b0f9fd2
Andi1218 <Andi-lenz@web.de>
2017-02-03 14:40:17 +0100

    spannungsdiagramm läuft

--- Website/js/charts.js
+++ Website/js/charts.js
@@ -1,19 +1,41 @@
 /* Chart current Voltage */
 
  
-     $(document).ready(function () {
+     function VoltageChart () {
       
       var dataLength = 15;
-           
+      var csvData;
       
-      var g = new Dygraph("div_g", "../temp/csv/alldata.csv",
-                          {
-                            drawPoints: true,
-                            showRoller: false,
-                            
-                            
-                          });   
-                          
+      $.ajax({
+                    type: "GET",
+                    url: "http://localhost:8080/temp/csv/alldata.csv",
+                    dataType: "text",
+                    success: function (data) {
+                       csvData = data;      
+                      
+                       VoltageGraph(csvData);
+                       //alert("done!"+ csvData.getAllResponseHeaders()) - my fix makes this won't work...
+                     }   
+                     
+                });
+      
+       
+        
+       
+      function VoltageGraph () { 
+      console.log(csvData);
+      var g = new Dygraph(document.getElementById("div_g"), csvData,
+              {
+                axis : {
+                  x : {
+                    valueFormatter: Dygraph.dateString_,
+                    valueParser: function(x) { return 1000*parseInt(x); },
+                    ticker: Dygraph.dateTicker                
+                  }
+                }
+              });
+                                  
+      }
       // It sucks that these things aren't objects, and we need to store state in window.
     /* window.intervalId = setInterval(function() {
         var x = new Date();  // current time
@@ -26,7 +48,7 @@
         };
       }, 1000);  */    
     }            
-);       /*
+      /*
 
 
  $(document).ready(function () {
--- Website/js/functions.js
+++ Website/js/functions.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     });  
     $("#U").click(function(){                                               /* makes the content of "Spannung" visible for the first time click, necessery because it doesn't show up, if
                                                                             it's initially hidden by css's "display: none" command */
-        document.getElementById("Spannung").style.visibility = "visible";   
+        VoltageChart();   
     });  
      
 });
