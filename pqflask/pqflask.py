75e42690d8ffc70a6f4d05c0e82832a827df8c6d
pi <ere86352@stud.uni-stuttgart.de>
2017-02-09 06:49:10 +0100

    fuck of flask

--- pqflask/pqflask.py
+++ pqflask/pqflask.py
@@ -1,9 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/flask')
+def test():
     return 'hello world'
+
+@app.route('/html/Historische-Werte.html', methods=['POST'])
+def get_data():
+    # parse POST request
+    requested_values = request.get_json()
+
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
