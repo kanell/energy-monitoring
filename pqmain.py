b0e1b16df81305d4a912c044756067545ea70673
kanell <maltegerber@gmail.com>
2017-02-21 15:10:51 +0100

    try to add multiprocessing for better performance

--- pqmain.py
+++ pqmain.py
@@ -15,13 +15,79 @@ import datetime as dt
 import json
 import os
 import analyse as ana
+from multiprocessing import Process, Queue, Value
 
+# processes
+def get_data(queue, dataframe, ports, pqid, control_flag):
+    # init
+    max_time = 0
+    min_time = 1000
+    timestamp = int(dt.datetime.now().timestamp())
+    
+    try:
+        while control_flag.value == 0:
+            time1 = time.time()
+
+            # get data from janitza
+            pq_data = gd.fetch_data_dataframe(dataframe, ports, pqid)
+            
+            # put in queue
+            queue.put((pq_data, timestamp))
+
+            time2 = time.time()
+            min_time = min(min_time,time2-time1)
+            max_time = max(max_time,time2-time1)
+            print('modbus loop duration time| current: {:6.4f} sec.\t| max: {:6.4f} sec.\t| min: {:6.4f} sec.'.format(time2-time1,max_time,min_time), end='\r')
+            # try to get data every 1 second
+            if time2-time1 <= timedelta:
+                timestamp += 2*timedelta
+                time.sleep(timedelta-(time2-time1))
+            else:
+                timestamp += 2*timedelta
+                time.sleep(2*timedelta-(time2-time1))
+    except:
+        control_flag.value = 1
+        
+        import traceback
+        print(traceback.format_exc())
+        raise
+    finally:
+        # check if queue is empty
+        while queue.qsize() != 0:
+            queue.get()
+            
+def put_data(queue, db, tablename, control_flag):
+    try:
+        while control_flag.value == 0:
+            if queue.qsize() == 0:
+                time.sleep(0.2)
+            else:
+                # get dict from queue
+                datadict = queue.get()
+                
+                # insert data in database
+                db.insert(tablename,datadict)
+    except:
+        control_flag.value = 1
+        
+        import traceback
+        print(traceback.format_exc())
+        raise
+    finally:
+        # check if queue is empty
+        while queue.qsize() != 0:
+            queue.get()
+            
 # init
 ipaddr = '129.69.176.123'
 timedelta = 2 # seconds
 tablename = 'pqdata'
 max_time = 0
 min_time = 1000
+control_flag = Value('i',0)
+# queues
+mbqueue = Queue()
+dbqueue = Queue()
 
 # create folders
 paths = ['temp', 'temp/jsons', 'temp/csv']
@@ -69,46 +135,57 @@ try:
     if not 'public.'+tablename in db.get_tables():
         pqdb.create_db_table(db,tablename,db_table_config)
         
+    # Start processes
+    args = (mbqueue, dataframe, ports, pqid, control_flag)
+    mbprocess = Process(target=get_data, args=args)
+    mbprocess.start()
+    print('Started modbus process')
+    args = (dbqueue, db, tablename, control_flag)
+    dbprocess = Process(target=put_data, args=args)
+    dbprocess.start()
+    print('Started database process')
+        
     timestamp = int(dt.datetime.now().timestamp())
     
-    while True:
-        time1 = time.time()
-        # get data from janitza
-        pq_data = gd.fetch_data_dataframe(dataframe, ports, pqid)
-        
-        # send data to analysis func
-        frequency_10s, status_dict = ana.analyse(pq_data)
-        
-        # create dict for database insert
-        for addr in pq_data.index:
-            datadict['port_'+str(addr)] = pq_data[addr]
-        
-        # add primary key to every dict and frequency 10s
-        datadict['timestamp'] = timestamp
-        datadict['frequency_10s'] = frequency_10s
-
-        # insert data in database
-        db.insert(tablename,datadict)
-
-        # create data json
-        with open('temp/jsons/alldata.json','w') as f:
-            f.write(json.dumps(datadict))
-        
-        time2 = time.time()
-        min_time = min(min_time,time2-time1)
-        max_time = max(max_time,time2-time1)
-        print('loop duration time| current: {:6.4f} sec.\t| max: {:6.4f} sec.\t| min: {:6.4f} sec.'.format(time2-time1,max_time,min_time), end='\r')
-        # try to get data every 1 second
-        if time2-time1 <= timedelta:
-            timestamp += timedelta
-            time.sleep(timedelta-time1+time2) 
+    while control_flag.value == 0:
+        if mbqueue.qsize() == 0:
+            time.sleep(0.2)
         else:
-            timestamp += 2*timedelta
-            time.sleep(2*timedelta-time1+time2)
+            time1 = time.time()
+            
+            # get data from mbqueue
+            pq_data = mbqueue.get()
+            
+            # send data to analysis func
+            frequency_10s, status_dict = ana.analyse(pq_data)
+            
+            # create dict for database insert
+            for addr in pq_data.index:
+                datadict['port_'+str(addr)] = pq_data[addr]
+            
+            # add primary key to every dict and frequency 10s
+            datadict['timestamp'] = timestamp
+            datadict['frequency_10s'] = frequency_10s
+    
+            # insert data in dbqueue
+            dbqueue.put(datadict)
+    
+            # create data json
+            with open('temp/jsons/alldata.json','w') as f:
+                f.write(json.dumps(datadict))
+            
+            time2 = time.time()
+            min_time = min(min_time,time2-time1)
+            max_time = max(max_time,time2-time1)
+            #print('main loop duration time| current: {:6.4f} sec.\t| max: {:6.4f} sec.\t| min: {:6.4f} sec.'.format(time2-time1,max_time,min_time), end='\r')
 
-except KeyboardInterrupt:
+except KeyboardInterrupt:        
+    control_flag.value = 1
+        
     print('\nCTRL-C KeyboardInterrupt is active')
 except:
+    control_flag.value = 1
+        
     import traceback
     print(traceback.format_exc())
     raise
@@ -120,4 +197,7 @@ finally:
         ps.strip_dirs()
         ps.sort_stats('cumtime')
         ps.dump_stats('profiling_pqmain.pstat')
+        
+    mbprocess.join()
+    dbprocess.join()
     
